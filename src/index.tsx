88f23377c05bb1b43e13d6ddc2e1c176fd45470a
郑瑜琨 <zyk185387@alibaba-inc.com>
2019-05-08 23:40:57 +0800

    字段 和 主表

--- src/index.tsx
+++ src/index.tsx
@@ -1,91 +1,81 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactJson from 'react-json-view'
 import recast from 'recast'
 import { rootGrpahqlCodeMaker } from './../src/creator'
 import { IDBQuery } from './../src/graphql-type'
 import './../test/creator.test'
+import { Db, User  } from './../test/db.test'
 
-const linqExpress1 = (db: any) => db.Users.filter((u: any) => u.Id === 4).map(({name}) => ({name}))
-const code = linqExpress1.toString()
-const ast = recast.parse(code)
-// const fun  = ast.program.body[0]
-// tslint:disable-next-line: no-console
-// console.log(code)
-// // tslint:disable-next-line:no-console
-// console.log(ast)
-let DBParam = ''
-const query: IDBQuery = { rootTable : { name: '', fields: []}}
-recast.visit(ast, {
-    // visitArrowFunctionExpression(a) {
-    //  if (a.name === 'expression') {
-    //     DBParam = a.value.params[0].name
-    //     console.log(a)
-    //  }
-    //  this.traverse(a)
-    // },
+type ILinqDb<TDb = any> = (db: TDb) => void
+declare global {
+    // tslint:disable-next-line:interface-name
+    interface Function {
+        gql(db?: any): string
+    }
+}
 
-    visitIdentifier(a) {
-        this.traverse(a)
-    },
-
+Function.prototype.gql = function (db?: any) {
+    const code = this.toString()
+    const ast = recast.parse(code)
+    let DBParam = ''
+    const query: IDBQuery = { rootTable : { name: '', fields: []}}
+    recast.visit(ast, {
     visitExpression(a) {
          if (a.name === 'expression') {
+          // 第一个传入参数
           DBParam = a.value.params[0].name
+          // 找到map 字段集合
+          const mapArgs =  a.value.body.arguments[0].body.properties.map((a) => a.key.name)
+          query.rootTable.fields = mapArgs.map((a) => ({ name : a}))
+
          }
-         if (a.name === 'callee') {
-            if (a!.value!.object!.object && a!.value!.object!.object!.name === DBParam)
-            query.rootTable.name = a.value.object.property.name
-         }
+
+         if (a.name === 'callee' && a!.value!.object!.object && a!.value!.object!.object!.name === DBParam)
+                query.rootTable.name = a.value.object.property.name
+
+        //  if (a.name === 'body' && a.parentPath.name ===  'expression') {
+
+        //  }
+
+        //  if (a.name === 'callee') {
+        //     // 第一个callee 一定是返回值，判断下是否是map
+        //     if (a.value.property && a.value.property.name === 'map') {
+
+        //     } else {
+        //         if (a!.value!.object!.object && a!.value!.object!.object!.name === DBParam)
+        //         query.rootTable.name = a.value.object.property.name
+        //     }
+
+        //  }
+         // tslint:disable-next-line:no-console
          console.log(a)
          this.traverse(a)
     },
 })
 
-if (query.rootTable.fields.length === 0) {
+    if (query.rootTable.fields.length === 0) {
     query.rootTable.fields = [{name: 'id'}, {name: 'name'}]
 }
-
-console.log(rootGrpahqlCodeMaker(query))
-
-export enum ExpressionType {
-
-}
-// alert(JSON.stringify(ast))
-// console.log(JSON.parse(JSON.stringify(ast)))
-// tslint:disable-next-line:no-console
-// console.log(recast.print(fun.expression.body.body[0].argument.arguments[0].body.body[0]).code)
-
-// tslint:disable-next-line:ban-types
-export class Expression<T extends Function> {
-    public readonly CanReduce: boolean
-    public Name: string
-    public Body: Expression<T>
-    public Parameters: string[]
-    public NodeType: ExpressionType
-   // tslint:disable-next-line:ban-types
-    constructor(public Type: T) {
-    const code = this.Type.toString()
-    const ast = recast.parse(code)
-    const ArrowFunctionExpression = ast.program.body[0].expression
-    this.Parameters = ArrowFunctionExpression.params.map((a: any) => a.name)
-    // ast.body[0]
-   }
-
-    public Compile() {
-       return this.Type
-   }
+    const res = rootGrpahqlCodeMaker(query)
+    return res
 }
 
-export interface IQueryProvider {
-    CreateQuery(expression: Expression<any>): IQueryable<any>
-    Execute(expression: Expression<any>): any
-}
+const LinqExpression1: ILinqDb<Db> = (db) =>
+ db.Users.filter((u) => u.Id === '4').map(({Name, Mobile}) => ({Name, Mobile}))
+console.log(LinqExpression1.gql())
 
-export interface IQueryable<T> extends Iterable<T> {
-
-    readonly Expression: Expression < any >
-    readonly Provider: IQueryProvider,
-}
-
-const expression2 = new Expression((a: number, b: number) => a + b)
-const fun2 = expression2.Compile()
-// alert(fun2(1, 2))
 // ---------------------------
+
+const App = () => {
+    const my_json_object = {aaa: 123}
+    return (
+    <>
+    <ReactJson src={{json : '123'}} />
+         </>)
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app'),
+  )
---
+++ test/db.test.ts
@@ -0,0 +1,11 @@
+
+export class User {
+   public Id: string
+   public Name: string
+   public Mobile: string
+}
+
+// tslint:disable-next-line: max-classes-per-file
+export class Db {
+   public Users: User[]
+}
