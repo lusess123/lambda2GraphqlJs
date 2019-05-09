11250d0c47e2e33c82ca165c43bc2e3bba26742e
郑瑜琨 <zyk185387@alibaba-inc.com>
2019-05-09 10:44:19 +0800

    gql

--- src/creator.ts
+++ src/creator.ts
@@ -3,7 +3,8 @@ import { IDBQuery } from './graphql-type'
 export const rootGrpahqlCodeMaker = (query: IDBQuery) => {
    return `
    {
-     ${query.rootTable.name} {
+     ${query.rootTable.name} ${query.rootTable.where ? `(${query.
+      rootTable.where[0].key} = ${query.rootTable.where[0].value})` : ''} {
          ${query.rootTable.fields.map((a) => {
            return  '\n       ' + a.name
          }).join(',')}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ReactJson from 'react-json-view'
 import recast from 'recast'
 import { rootGrpahqlCodeMaker } from './../src/creator'
 import { IDBQuery } from './../src/graphql-type'
-import './../test/creator.test'
-import { Db, User  } from './../test/db.test'
 
-type ILinqDb<TDb = any> = (db: TDb) => void
+export type ILinqDb<TDb = any> = (db: TDb) => void
 declare global {
     // tslint:disable-next-line:interface-name
     interface Function {
@@ -21,61 +16,33 @@ Function.prototype.gql = function (db?: any) {
     let DBParam = ''
     const query: IDBQuery = { rootTable : { name: '', fields: []}}
     recast.visit(ast, {
-    visitExpression(a) {
-         if (a.name === 'expression') {
-          // 第一个传入参数
-          DBParam = a.value.params[0].name
-          // 找到map 字段集合
-          const mapArgs =  a.value.body.arguments[0].body.properties.map((a) => a.key.name)
-          query.rootTable.fields = mapArgs.map((a) => ({ name : a}))
-
-         }
-
-         if (a.name === 'callee' && a!.value!.object!.object && a!.value!.object!.object!.name === DBParam)
-                query.rootTable.name = a.value.object.property.name
-
-        //  if (a.name === 'body' && a.parentPath.name ===  'expression') {
-
-        //  }
-
-        //  if (a.name === 'callee') {
-        //     // 第一个callee 一定是返回值，判断下是否是map
-        //     if (a.value.property && a.value.property.name === 'map') {
-
-        //     } else {
-        //         if (a!.value!.object!.object && a!.value!.object!.object!.name === DBParam)
-        //         query.rootTable.name = a.value.object.property.name
-        //     }
-
-        //  }
-         // tslint:disable-next-line:no-console
-         console.log(a)
-         this.traverse(a)
-    },
-})
+                  visitExpression(a) {
+                    if (a.name === 'expression') {
+                      // 第一个传入参数
+                       DBParam = a.value.params[0].name
+                        // 找到map 字段集合
+                       if (a.value.body.callee.property.name === 'map') {
+                           const mapArgs =  a.value.body.arguments[0].body.properties.map((a) => a.key.name)
+                           query.rootTable.fields = mapArgs.map((a) => ({ name : a}))
+                        }
+                    }
+                    if (a.name === 'callee' && a!.value!.object!.object && a!.value!.object!.object!.name === DBParam) {
+                       query.rootTable.name = a.value.object.property.name
+                       if (a.value.property.name === 'filter') {
+                          if (a.parentPath.value.arguments[0].body.operator === '===') {
+                            const filterName = a.parentPath.value.arguments[0].body.left.property.name
+                            const filterValue = a.parentPath.value.arguments[0].body.right.value
+                            query.rootTable.where = [{ key: filterName , value: filterValue}]
+                          }
+                        }
+                    }
+                    this.traverse(a)
+                  },
+    })
 
     if (query.rootTable.fields.length === 0) {
-    query.rootTable.fields = [{name: 'id'}, {name: 'name'}]
-}
+        query.rootTable.fields = [{name: 'id'}, {name: 'name'}]
+    }
     const res = rootGrpahqlCodeMaker(query)
     return res
 }
-
-const LinqExpression1: ILinqDb<Db> = (db) =>
- db.Users.filter((u) => u.Id === '4').map(({Name, Mobile}) => ({Name, Mobile}))
-console.log(LinqExpression1.gql())
-
-// ---------------------------
-
-const App = () => {
-    const my_json_object = {aaa: 123}
-    return (
-    <>
-    <ReactJson src={{json : '123'}} />
-         </>)
-}
-
-ReactDOM.render(
-    <App/>,
-    document.getElementById('app'),
-  )
