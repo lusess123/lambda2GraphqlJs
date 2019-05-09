350861c4ea13af679a69f485a5823a58221396dd
郑瑜琨 <zyk185387@alibaba-inc.com>
2019-05-09 15:05:04 +0800

    test code

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,54 @@
 import recast from 'recast'
-import { rootGrpahqlCodeMaker } from './../src/creator'
-import { IDBQuery } from './../src/graphql-type'
+import { rootGrpahqlCodeMaker } from './creator'
+import { IDBQuery } from './graphql-type'
 
 export type ILinqDb<TDb = any> = (db: TDb) => void
+export function gql<TDb = any>(fun: ILinqDb<TDb>): string {
+  const code = fun.toString()
+  const ast = recast.parse(code)
+  let DBParam = ''
+  const query: IDBQuery = { rootTable : { name: '', fields: []}}
+  recast.visit(ast, {
+                visitExpression(a) {
+                  if (a.name === 'expression') {
+                    // 第一个传入参数
+                     DBParam = a.value.params[0].name
+                      // 找到map 字段集合
+                     if (a.value.body.callee.property.name === 'map') {
+                         const mapArgs =  a.value.body.arguments[0].body.properties.map((a) => a.key.name)
+                         query.rootTable.fields = mapArgs.map((a) => ({ name : a}))
+                      }
+                  }
+                  if (a.name === 'callee' && a!.value!.object!.object && a!.value!.object!.object!.name === DBParam) {
+                     query.rootTable.name = a.value.object.property.name
+                     if (a.value.property.name === 'filter') {
+                        if (a.parentPath.value.arguments[0].body.operator === '===') {
+                          const filterName = a.parentPath.value.arguments[0].body.left.property.name
+                          const filterValue = a.parentPath.value.arguments[0].body.right.value
+                          query.rootTable.where = [{ key: filterName , value: filterValue}]
+                        }
+                      }
+                  }
+                  this.traverse(a)
+                },
+  })
+
+  // if (query.rootTable.fields.length === 0) {
+  //     query.rootTable.fields = [{name: 'id'}, {name: 'name'}]
+  // }
+  const res = rootGrpahqlCodeMaker(query)
+  return res
+}
+
+export class Lambda<TDb = any> {
+
+     constructor(private fun: ILinqDb<TDb>) {
+
+     }
+     public gql(): string {
+      return gql(this.fun)
+    }
+}
 declare global {
     // tslint:disable-next-line:interface-name
     interface Function {
@@ -10,39 +56,9 @@ declare global {
     }
 }
 
-Function.prototype.gql = function (db?: any) {
-    const code = this.toString()
-    const ast = recast.parse(code)
-    let DBParam = ''
-    const query: IDBQuery = { rootTable : { name: '', fields: []}}
-    recast.visit(ast, {
-                  visitExpression(a) {
-                    if (a.name === 'expression') {
-                      // 第一个传入参数
-                       DBParam = a.value.params[0].name
-                        // 找到map 字段集合
-                       if (a.value.body.callee.property.name === 'map') {
-                           const mapArgs =  a.value.body.arguments[0].body.properties.map((a) => a.key.name)
-                           query.rootTable.fields = mapArgs.map((a) => ({ name : a}))
-                        }
-                    }
-                    if (a.name === 'callee' && a!.value!.object!.object && a!.value!.object!.object!.name === DBParam) {
-                       query.rootTable.name = a.value.object.property.name
-                       if (a.value.property.name === 'filter') {
-                          if (a.parentPath.value.arguments[0].body.operator === '===') {
-                            const filterName = a.parentPath.value.arguments[0].body.left.property.name
-                            const filterValue = a.parentPath.value.arguments[0].body.right.value
-                            query.rootTable.where = [{ key: filterName , value: filterValue}]
-                          }
-                        }
-                    }
-                    this.traverse(a)
-                  },
-    })
-
-    if (query.rootTable.fields.length === 0) {
-        query.rootTable.fields = [{name: 'id'}, {name: 'name'}]
+export const extendFunction = () => {
+   if (!Function.prototype.gql)
+   Function.prototype.gql = function() {
+      return gql(this)
     }
-    const res = rootGrpahqlCodeMaker(query)
-    return res
 }
---
+++ test/index.ts
@@ -0,0 +1,17 @@
+import { extendFunction, gql, ILinqDb, Lambda } from './../src/index'
+import { Db} from './db.test'
+
+const code = gql((a) =>  a.Users.map(({ Id, Mobile, Name}) => ({ Mobile, Name, Id})))
+// tslint:disable-next-line:no-console
+console.log(code)
+
+const lambda1 = new Lambda<Db>(
+  ((a) =>  a.Users.filter((a) => a.Mobile === '1232323').map(({ Id, Mobile, Name}) => ({ Mobile}))),
+  )
+// tslint:disable-next-line:no-console
+console.log(lambda1.gql())
+
+extendFunction()
+const lambda: ILinqDb<Db> = (a) =>  a.Users.map(({ Id, Mobile, Name}) => ({ Mobile, Name}))
+// tslint:disable-next-line:no-console
+console.log(lambda.gql())
